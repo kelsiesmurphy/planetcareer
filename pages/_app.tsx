f59c5140eba1317501895832e3297efcb5125933
Kelsie Murphy <kelsie@kelsiesmurphy.com>
2023-05-15 00:36:02 +0100

    Adding analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,21 +1,26 @@
-import '../styles/globals.css';
-import { useState } from 'react'
-import { createBrowserSupabaseClient } from '@supabase/auth-helpers-nextjs'
-import { SessionContextProvider, Session } from '@supabase/auth-helpers-react'
-import { AppProps } from 'next/app'
+import "../styles/globals.css";
+import { useState } from "react";
+import { createBrowserSupabaseClient } from "@supabase/auth-helpers-nextjs";
+import { SessionContextProvider, Session } from "@supabase/auth-helpers-react";
+import { AppProps } from "next/app";
+import { Analytics } from "@vercel/analytics/react";
 
 function MyApp({
   Component,
   pageProps,
 }: AppProps<{
-  initialSession: Session,
+  initialSession: Session;
 }>) {
-  const [supabase] = useState(() => createBrowserSupabaseClient())
+  const [supabase] = useState(() => createBrowserSupabaseClient());
 
   return (
-    <SessionContextProvider supabaseClient={supabase} initialSession={pageProps.initialSession}>
+    <SessionContextProvider
+      supabaseClient={supabase}
+      initialSession={pageProps.initialSession}
+    >
       <Component {...pageProps} />
+      <Analytics />
     </SessionContextProvider>
-  )
+  );
 }
-export default MyApp
+export default MyApp;
