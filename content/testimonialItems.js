1f3b1b9a58d6e47c09dd36cffc5391c6dbfcc4e6
Kelsie Murphy <kelsie@kelsiesmurphy.com>
2023-05-30 23:26:08 +0100

    Adding testimonial item

--- content/testimonialItems.js
+++ content/testimonialItems.js
@@ -11,4 +11,10 @@ export const testimonialItems = [
     review:
       "Sounds very interesting and I am sure will be a lot of value to a lot of people currently applying for work. Can be challenging to keep yourself organised",
   },
+  {
+    id: 3,
+    name: "Kirsty",
+    review:
+      "I am just beginning to apply to jobs and thought I'd crack out a classic spreadsheet until I saw that one of my CodeClan instructors had shared your app with us and it's so useful",
+  },
 ];
