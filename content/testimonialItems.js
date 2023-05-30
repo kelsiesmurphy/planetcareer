15e2ba0254e19adc17ea41fd08450da1539cd372
Kelsie Murphy <kelsie@kelsiesmurphy.com>
2023-05-30 18:30:43 +0100

    adding testimonials ui

---
+++ content/testimonialItems.js
@@ -0,0 +1,14 @@
+export const testimonialItems = [
+  {
+    id: 1,
+    name: "Ross",
+    review:
+      "The career tracker is incredible ! I was using google sheets before but the UI of your app is so much nicer haha great job!",
+  },
+  {
+    id: 2,
+    name: "Emma",
+    review:
+      "Sounds very interesting and I am sure will be a lot of value to a lot of people currently applying for work. Can be challenging to keep yourself organised",
+  },
+];
